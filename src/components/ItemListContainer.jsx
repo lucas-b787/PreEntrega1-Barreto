32b33085e98e898148b219866a47d0609da4e373
lucas <lucas.b787@hotmail.com>
2025-03-12 13:29:04 -0300

    nueva api

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,26 +3,18 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export const ItemListContainer = () => {
-  const [resultado, setResultado] = useState([]);
+  const [responce, setResponce] = useState([]);
   const params = useParams();
-  const url = "https://api.catalog.beer"; //https://catalog.beer/api-docs#beer-object
-  const apiKey =
-    "curl --location --request GET 'https://api.catalog.beer/brewer'";
-  (" --header Accept: application/json");
-  (" --header Authorization: Basic 7152aca7-5df0-4e1b-985f-ce204fb63775");
+  const url = "https://dummyjson.com/products";
 
-  const options = useEffect(() => {
-    fetch(url, apiKey)
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
       .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setResultado(res);
         console.log(res);
-      })
-      .catch();
-    console.log("hubo un error");
-  });
+        setResponce(res);
+      });
+  }, []);
 
   return <div></div>;
 };
